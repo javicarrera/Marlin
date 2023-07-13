d50c65bffa67c81510fe3cbf9556453a01aeca90
javicarrera <javicarrera@gmail.com>
2023-07-13 20:25:19 +0200

    MESH MAX

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2306,8 +2306,8 @@
   // Override the mesh area if the automatic (max) area is too large
   #define MESH_MIN_X MESH_INSET
   #define MESH_MIN_Y MESH_INSET
-  #define MESH_MAX_X X_BED_SIZE - (MESH_INSET)
-  #define MESH_MAX_Y Y_BED_SIZE - (MESH_INSET)
+  #define MESH_MAX_X X_BED_SIZE - (MESH_INSET) - 40
+  #define MESH_MAX_Y Y_BED_SIZE - (MESH_INSET) + 8
 #endif
 
 #if ALL(AUTO_BED_LEVELING_UBL, EEPROM_SETTINGS)
